96c8759799e8589f4d2f8590aec5409df521fbc3
Peter Neubauer <peter@neubauer.se>
2014-03-09 16:42:46 +0100

    initial commit

---
+++ geteventstore.rb
@@ -0,0 +1,11 @@
+class Eventstore < Formula
+  url 'http://download.geteventstore.com/binaries/eventstore-mono-2.0.1.tgz'
+  homepage 'http://geteventstore.com/'
+  sha1 'e356280ae45beaac6476824d551b094cd12e03b9'
+  version '2.0.1'
+
+  #def install
+    # Symlink binaries
+  #  bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell}"]
+  #end
+end
