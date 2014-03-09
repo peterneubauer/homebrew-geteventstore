016a488970be91c051111f49215d7bbc9e4758d0
Peter Neubauer <peter@neubauer.se>
2014-03-09 17:57:17 +0100

    working formula

--- geteventstore.rb
+++ geteventstore.rb
@@ -6,8 +6,7 @@ class Geteventstore < Formula
   sha1 '99744c57fb9930df96424e9ef7c73490e60ab9fb'
   version '2.0.1'
 
-  #def install
-    # Symlink binaries
-  #  bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell}"]
-  #end
+  def install
+    bin.install Dir['*']
+  end
 end
